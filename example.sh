0922791ed782daa320722eeaee2bb781cd5c3a94
I65114 <bbgroesbeck@verisk.com>
2024-01-10 08:32:13 -0700

    whatever

---
+++ example.sh
@@ -0,0 +1,13 @@
+cd ~/Documents/mtechjan24/day2gitstuff/
+#git clone #repo name here
+#cd #repo from step 2
+# mkdir day2-example
+# cd day2-example
+# echo "Starting my script $(date)"
+# echo "*DS_Store" > .gitignore
+# touch index.html
+touch main.css
+touch app.js
+mkdir css
+mv *.css css/
+tree 
